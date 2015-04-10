ed738b7356a137488ea459d6941f0fc87f95e420
guyjstitt <guyjstitt@gmail.com>
2015-04-10 00:21:42 -0400

    created server routes folder, cleaned up index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express 			= require('express'),
  	app 				= express(),
  	bodyParser			= require('body-parser'),
  	mongoose			= require('mongoose'),
- 	Meetup              = require('./server/models/meetup');
+ 	meetup				= require('./server/routes/meetup');
 
 var mongoURI = 'mongodb://localhost:27017/mean-demo';
 app.set('port', (process.env.PORT || 5000));
@@ -19,9 +19,6 @@ app.use('/js', express.static(__dirname + '/client/js'));
 //app.get('/api/meetups', meetupsController.list)
 //app.post('/api/meetups', meetupsController.create);
 
-//ROUTES FOR API
-var router = express.Router();
-
 // router.user(function(req, res, next) {
 // 	next();
 // });
@@ -30,70 +27,9 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/client/views/index.html');
 });
 
-// route middleware that will happen on every request
-router.use(function(req, res, next) {
+app.use('/api', meetup);
 
-    // log each request to the console
-    console.log(req.method, req.url);
-
-    // continue doing what we were doing and go to the route
-    next(); 
-});
-
-router.route('/meetups')
-	.post(function(req, res) {
-		var meetup = new Meetup();
-		meetup.name = req.body.name;
-
-		meetup.save(function(err) {
-			if(err) {
-				res.send(err);
-			} else {
-				res.json({message: 'Meetup Created'});
-			}
-		});
-	})
-
-	.get(function(req, res) {
-		Meetup.find(function(err, meetups) {
-			if(err) {
-				res.send(err);
-			} else {
-				res.json(meetups);
-			}
-
-		});
-	});
-
-router.route('/meetups/:_id/edit')
-	.get(function(req, res) {
-		 Meetup.findById(req.params._id ,function(err, meetup) {
-			if(err) {
-				return console.error(err);
-			} else {
-				console.log('edit successful', meetup);
-			}
-		});
-	});
-
-router.route('/meetups/:_id/delete')
-	.delete(function(req, res) {
-		 Meetup.findById(req.params._id ,function(err, meetup) {
-			if(err) {
-				return console.error(err);
-			} else {
-				meetup.remove(function(err, meetup) {
-					if(err) {
-						console.error(err);
-					} else {
-						console.log("deleted " + meetup._id);
-					}
-				})
-			}
-		})
-	});
-
-app.use('/api', router);
+console.log(meetup);
 
 
 //PORT
