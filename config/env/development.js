ab3db6c0ae65eff1df05cc99c4bb1f3b0edff7a8
haowhen <hnnguy02@louisville.edu>
2015-04-21 12:10:52 -0400

    Fixed mongoURI

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 var port = process.env.PORT || 5000;
-var mongoUri = 'mongodb://localhost:27017/mean-demo';
+var mongoURI = 'mongodb://localhost:27017/mean-demo';
 var db = process.env.MONGOLAB_URI || mongoURI;
 
 module.exports = {
