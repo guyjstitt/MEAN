a0b7522b7530fd1f1ca69c0c175dac2961dfc5d9
haowhen <hnnguy02@louisville.edu>
2015-04-29 14:02:48 -0400

    Added dropdown for profile and logout

--- server/routes/index.js
+++ server/routes/index.js
@@ -22,7 +22,7 @@ module.exports = function(app) {
 		} else {
 			console.log(req.user);
 			res.render('default', 
-				{ title: 'temp',
+				{ title: 'My Events',
 					user: JSON.stringify(req.user)}
 			)
 		}
